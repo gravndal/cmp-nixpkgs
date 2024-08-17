a5035bdd05a25adbf3d6bcb6226422b1db8cc987
Gaute Ravndal <gaute.ravndal@gmail.com>
2024-08-17 23:39:04 +0200

    Use vim.system in utils.manix.query

--- lua/cmp_nixpkgs/utils/manix.lua
+++ lua/cmp_nixpkgs/utils/manix.lua
@@ -3,12 +3,12 @@ local M = {}
 if vim.fn.executable('manix') ~= 0 and vim.fn.executable('pwait') ~= 0 then
   -- wait on running instances of manix before calling function
   local function pwait(fn)
-    vim.fn.jobstart({ 'pwait', '--exact', 'manix' }, { on_exit = fn })
+    vim.system({ 'pwait', '--exact', 'manix' }, {}, fn)
   end
 
   -- precache on startup if possible
   pwait(function()
-    vim.fn.jobstart({
+    vim.system({
       'manix',
       'programs.less',
       '--source',
@@ -18,20 +18,16 @@ if vim.fn.executable('manix') ~= 0 and vim.fn.executable('pwait') ~= 0 then
 
   M.query = function(query, source, completion_item, callback)
     pwait(function()
-      -- return early if the completion menu is already closed
-      if not require('cmp').get_active_entry() then
-        return callback(completion_item)
-      end
-      vim.fn.jobstart(
+      vim.system(
         source and { 'manix', '-s', query, '--source', source }
           or { 'manix', '-s', query },
         {
-          stdout_buffered = true,
-          on_stdout = function(_, data)
-            completion_item.detail = table.concat(data, '\n')
-            return callback(completion_item)
-          end,
-        }
+          text = true,
+        },
+        function(obj)
+          completion_item.detail = obj.stdout
+          return callback(completion_item)
+        end
       )
     end)
   end
